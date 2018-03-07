4e94887133baf7be841ce2212bf07041e607bb52
ouyang chao <chao@ouyang.io>
2018-03-07 14:16:06 +0800

    bug fixes

--- demo/dist/bundle.js
+++ demo/dist/bundle.js
@@ -70,23 +70,26 @@
 "use strict";
 
 
-var _jsxDomRender = _interopRequireDefault(__webpack_require__(1));
+var _index = _interopRequireDefault(__webpack_require__(1));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+// import React from 'jsx-dom-render';
 var h1Content = 'world';
 var list = ['Zhao', 'Qian', 'Sun', 'Li'];
 var btn;
-document.body.appendChild(_jsxDomRender.default.createElement("div", null, _jsxDomRender.default.createElement("h1", {
+document.body.appendChild(_index.default.createElement("div", {
+  "class": "cls1 cls2"
+}, _index.default.createElement("h1", {
   title: "hello world"
-}, "hello ", h1Content), _jsxDomRender.default.createElement("hr", null), _jsxDomRender.default.createElement("ol", null, list.map(function (item) {
-  return _jsxDomRender.default.createElement("li", null, item);
-})), _jsxDomRender.default.createElement("hr", null), _jsxDomRender.default.createElement("button", {
+}, "hello ", h1Content), _index.default.createElement("hr", null), _index.default.createElement("ol", null, list.map(function (item) {
+  return _index.default.createElement("li", null, item);
+})), _index.default.createElement("hr", null), _index.default.createElement("button", {
   ref: function ref(_) {
     return void (btn = _);
   },
   onClick: function onClick(e) {
-    return void alert('haha');
+    return void alert('from jsx');
   }
 })));
 btn.textContent = 'Click me';
@@ -96,43 +99,56 @@ btn.textContent = 'Click me';
 /***/ (function(module, exports) {
 
 const convertToTextNodeIfStr = node => typeof node === 'string' ? document.createTextNode(node) : node;
-const createElement = function (tag, props) {
-    const ele = document.createElement(tag);
-    const children = Array.from(arguments).slice(2);
-    if (props) {
-        Object.entries(props).forEach(function (entry) {
-            entry[0] = entry[0].toLowerCase();
-            if (entry[0] === 'ref') {
-                entry[1](ele);
-                return;
-            }
-            if (entry[0] === 'classname') {
-                entry[0] = 'class';
-            }
-            if (entry[0] in ele) {
-                if (entry[0].slice(0, 2) === 'on') {
-                    ele.addEventListener(entry[0].slice(2), entry[1]);
-                    return;
-                }
-                ele.setAttribute(entry[0], entry[1]);
-            }
-        });
-    }
-    if (children) {
-        if (children.length === 1 && Array.isArray(children[0])) {
-            children.pop().forEach(function (child) {
-                children.push(convertToTextNodeIfStr(child));
-            });
-        }
-        children.forEach(function (child) {
-            ele.appendChild(convertToTextNodeIfStr(child));
-        });
-    }
-    return ele;
-};
-React = { createElement };
-module.exports = React;
 
+const createElement = function (tag, props) {
+  const ele = document.createElement(tag);
+  const children = Array.from(arguments).slice(2);
+
+  if (props) {
+    Object.entries(props).forEach(function (entry) {
+      entry[0] = entry[0].toLowerCase();
+
+      if (entry[0] === 'ref') {
+        entry[1](ele);
+        return;
+      }
+
+      if (entry[0] === 'classname') {
+        entry[0] = 'class';
+      }
+
+      if (entry[0] === 'class') {
+        entry[1].split(' ').forEach(cls => ele.classList.add(cls));
+      } else if (entry[0] in ele) {
+        if (entry[0].slice(0, 2) === 'on') {
+          ele.addEventListener(entry[0].slice(2), entry[1]);
+          return;
+        }
+
+        ele.setAttribute(entry[0], entry[1]);
+      }
+    });
+  }
+
+  if (children) {
+    if (children.length === 1 && Array.isArray(children[0])) {
+      children.pop().forEach(function (child) {
+        children.push(convertToTextNodeIfStr(child));
+      });
+    }
+
+    children.forEach(function (child) {
+      ele.appendChild(convertToTextNodeIfStr(child));
+    });
+  }
+
+  return ele;
+};
+
+React = {
+  createElement
+};
+module.exports = React;
 
 /***/ })
 /******/ ]);
