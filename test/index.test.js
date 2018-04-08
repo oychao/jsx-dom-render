b8dd914b4e80a88cab78ef53484408523d337f50
Ouyang Chao <ch.k.ouyang@gmail.com>
2018-04-08 18:41:36 +0800

    basci test

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,17 @@
 const assert = require('assert');
 
-describe('test jsx-dom-render:', () => {
-    it('should pass travis building.', done => {
+const React = require('..');
+
+describe('jsx-dom-render', () => {
+    it('should contains a method named createElement.', done => {
+        assert(typeof React.createElement === 'function');
         done();
     });
+
+    // // still working on how to test DOM related methods
+    // it('should be able to create DOM elements.', done => {
+    //     const div = React.createElement('div');
+    //     assert(div instanceof HTMLDivElement);
+    //     done();
+    // });
 });
